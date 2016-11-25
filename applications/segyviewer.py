ccdcc80657dba2e7fd36d5b09a0e1969bcb634f7
Jean-Paul Balabanian <jepebe@users.noreply.github.com>
2016-11-25 07:03:33 +0100

    Added support for saving the figure to image

--- applications/segyviewer.py
+++ applications/segyviewer.py
@@ -2,15 +2,16 @@
 import sys
 
 from PyQt4.QtCore import Qt
-from PyQt4.QtGui import QMainWindow, QApplication, QCheckBox
+from PyQt4.QtGui import QMainWindow, QApplication, QCheckBox, QToolButton, QFileDialog
 
 import segyio
 
-from segyviewlib import SliceViewWidget, ColormapCombo, SliceModel, SliceDirection as SD, SliceDataSource, LayoutCombo
+from segyviewlib import SliceViewWidget, ColormapCombo, SliceModel, SliceDirection as SD, SliceDataSource, LayoutCombo, \
+    resource_icon
 
 
 class SegyViewer(QMainWindow):
-    def __init__(self, s):
+    def __init__(self, s=None):
         QMainWindow.__init__(self)
 
         self.setAttribute(Qt.WA_DeleteOnClose)
@@ -32,16 +33,21 @@ class SegyViewer(QMainWindow):
         toolbar.addWidget(layout_combo)
         layout_combo.layout_changed.connect(self._slice_view_widget.set_plot_layout)
 
-        self._colormap_combo = ColormapCombo()
         # self._colormap_combo = ColormapCombo(['seismic', 'spectral', 'RdGy', 'hot', 'jet', 'gray'])
-
+        self._colormap_combo = ColormapCombo()
         self._colormap_combo.currentIndexChanged[int].connect(self._colormap_changed)
-
         toolbar.addWidget(self._colormap_combo)
 
         indicator_visibility = QCheckBox("Indicators")
         indicator_visibility.toggled.connect(self._slice_view_widget.show_indicators)
         toolbar.addWidget(indicator_visibility)
+
+        save_button = QToolButton()
+        save_button.setToolTip("Save as image")
+        save_button.setIcon(resource_icon("table_export.png"))
+        save_button.clicked.connect(self._save_figure)
+        toolbar.addWidget(save_button)
+
 
         indicator_visibility.setChecked(True)
         self._colormap_combo.setCurrentIndex(45)
@@ -52,6 +58,16 @@ class SegyViewer(QMainWindow):
     def _colormap_changed(self, index):
         colormap = str(self._colormap_combo.itemText(index))
         self._slice_view_widget.set_colormap(colormap)
+
+    def _save_figure(self):
+        formats = "Portable Network Graphic (*.png);;Adobe Acrobat (*.pdf);;Scalable Vector Graphics (*.svg)"
+        output_file = QFileDialog.getSaveFileName(self, "Save as image", "untitled.png", formats)
+
+        output_file = str(output_file).strip()
+
+        if len(output_file) > 0:
+            layout_figure = self._slice_view_widget.layout_figure()
+            layout_figure.savefig(output_file)
 
 
 def run(filename):
